ae59429803ef8471852aa0190f99d924d021ce9c
Sina Ibn Amin <sinaibnamin@Sinas-MacBook-Pro.local>
2020-11-23 18:17:01 +0600

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,7 @@
 
         $('html, body').animate({
           scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
 
         if ($(this).parents('.nav-menu, .mobile-nav').length) {
           $('.nav-menu .active, .mobile-nav .active').removeClass('active');
